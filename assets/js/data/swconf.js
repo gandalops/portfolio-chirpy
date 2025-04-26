9ff2ac0045be65a8bf51e1cf34790a34695b68ac
devyogi7579 <devyogi7579@users.noreply.github.com>
2025-04-26 08:56:21 +0000

    deploy: e781378a6de1a0b8f8d53151e855775fdc5e2230

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,7 +1,6 @@
-<<<<<<< HEAD
 const swconf = {
   
-    cacheName: 'chirpy-1745602781',resources: [
+    cacheName: 'chirpy-1745657780',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
@@ -22,58 +21,10 @@ const swconf = {
         
       ],urlPrefixes: [
         
-=======
----
-layout: compress
-permalink: '/:path/swconf.js'
-# Note that this file will be fetched by the ServiceWorker, so it will not be cached.
----
-
-const swconf = {
-  {% if site.pwa.cache.enabled %}
-    cacheName: 'chirpy-{{ "now" | date: "%s" }}',
-
-    {%- comment -%} Resources added to the cache during PWA installation. {%- endcomment -%}
-    resources: [
-      '{{ "/assets/css/:THEME.css" | replace: ':THEME', site.theme | relative_url }}',
-      '{{ "/" | relative_url }}',
-      {% for tab in site.tabs %}
-        '{{- tab.url | relative_url -}}',
-      {% endfor %}
-
-      {% assign cache_list = site.static_files | where: 'swcache', true %}
-      {% for file in cache_list %}
-        '{{ file.path | relative_url }}'{%- unless forloop.last -%},{%- endunless -%}
-      {% endfor %}
-    ],
-
-    interceptor: {
-      {%- comment -%} URLs containing the following paths will not be cached. {%- endcomment -%}
-      paths: [
-        {% for path in site.pwa.cache.deny_paths %}
-          {% unless path == empty %}
-            '{{ path | relative_url }}'{%- unless forloop.last -%},{%- endunless -%}
-          {% endunless  %}
-        {% endfor %}
-      ],
-
-      {%- comment -%} URLs containing the following prefixes will not be cached. {%- endcomment -%}
-      urlPrefixes: [
-        {% if site.analytics.goatcounter.id != nil and site.pageviews.provider == 'goatcounter' %}
-          'https://{{ site.analytics.goatcounter.id }}.goatcounter.com/counter/'
-        {% endif %}
->>>>>>> main
       ]
     },
 
     purge: false
-<<<<<<< HEAD
   
 };
 
-=======
-  {% else %}
-    purge: true
-  {% endif %}
-};
->>>>>>> main
