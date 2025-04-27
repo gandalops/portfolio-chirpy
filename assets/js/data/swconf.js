92505a6e10c261d2bbc4a06ed60e8e86b1017b86
devyogi7579 <devyogi7579@users.noreply.github.com>
2025-04-27 11:04:49 +0000

    deploy: d3b5e894c27d7bc6ec80697c044b5acbbabe913e

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1745711433',resources: [
+    cacheName: 'chirpy-1745751889',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
